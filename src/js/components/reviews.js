4a945e8c5ced9ec020645004d19c23d2a25e4fb2
makkons <justin.off@gmail.com>
2026-02-15 21:07:14 +0300

    refactoring

--- src/js/components/reviews.js
+++ src/js/components/reviews.js
@@ -1,28 +1,35 @@
 import Swiper from 'swiper'
-import { Autoplay } from 'swiper/modules'
+import { Autoplay, Pagination } from 'swiper/modules'
 import 'swiper/css'
 
 export function reviewsInit() {
 	const el = document.querySelector('.reviews__slider')
 	if (!el) return
 
-	new Swiper(el, {
-		modules: [Autoplay],
+	const swiper = new Swiper(el, {
+		modules: [Autoplay, Pagination],
 		loop: true,
 		speed: 600,
+		autoHeight: true,
 		autoplay: {
 			delay: 4000,
 			disableOnInteraction: false,
 		},
+		pagination: {
+			el: '.reviews__pagination',
+			clickable: true,
+		},
 		breakpoints: {
 			0: {
 				slidesPerView: 1,
-				spaceBetween: 16,
+				spaceBetween: 20,
 			},
 			769: {
 				slidesPerView: 2,
-				spaceBetween: 28,
+				spaceBetween: 40,
 			},
 		},
 	})
+
+	document.fonts.ready.then(() => swiper.update())
 }
