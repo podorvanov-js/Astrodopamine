69f0355433132a727605a48c50f0a16f017965d9
makkons <justin.off@gmail.com>
2026-02-16 15:19:45 +0300

    changed border hover color and remove js hover on price card

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { burgerInit } from './components/burger.js'
 import { footerInit } from './components/footer.js'
 import { faqInit } from './components/faq.js'
-import { pricesInit } from './components/prices.js'
+// import { pricesInit } from './components/prices.js'
 import { videoBgInit } from './components/video-bg.js'
 import { reviewsInit } from './components/reviews.js'
 import { scrollSpyInit } from './components/scroll-spy.js'
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	burgerInit();
 	footerInit();
 	faqInit();
-	pricesInit();
+	// pricesInit();
 	videoBgInit();
 	reviewsInit();
 	scrollSpyInit();
